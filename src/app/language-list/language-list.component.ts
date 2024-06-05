4e7bf4b082b78d66ffc5c303fa4ad873b0dd82db
Michael Lacy <mjlacy.dude@gmail.com>
2024-06-05 18:21:51 -0500

    Simplify

--- src/app/language-list/language-list.component.ts
+++ src/app/language-list/language-list.component.ts
@@ -1,6 +1,5 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 import { Language, Languages } from '../shared/models/language.model';
 import { LanguageService } from '../services/language.service';
 import { MatTableDataSource } from '@angular/material/table';
@@ -11,7 +10,7 @@ import { MatPaginator } from '@angular/material/paginator';
   templateUrl: './language-list.component.html',
   styleUrl: './language-list.component.scss'
 })
-export class LanguageListComponent implements OnInit, AfterViewInit {
+export class LanguageListComponent implements AfterViewInit {
   displayedColumns: string[] = ['name', 'creators', 'extensions', 'firstAppeared', 'year', 'wiki', 'edit', 'delete'];
   dataSource: MatTableDataSource<Language> = new MatTableDataSource<Language>([]);
 
@@ -19,22 +18,17 @@ export class LanguageListComponent implements OnInit, AfterViewInit {
 
   @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
 
-  ngOnInit(): void {
-    this.getLanguages();
-  }
-
   ngAfterViewInit(): void {
     this.dataSource.paginator = this.paginator as MatPaginator;
+
+    this.getLanguages();
   }
 
   getLanguages(): void {
     this.languageService.getLanguages()
-      .pipe(
-        map((languages: Languages) => languages?.languages)
-      ) 
       .subscribe({
-        next: (languages: Language[]) => {
-          this.dataSource.data = languages;
+        next: (languages: Languages) => {
+          this.dataSource.data = languages.languages;
       },
         error: (error: HttpErrorResponse) => {
           console.log(error);
