3a19eafc40dd85a46efe443ca3cc6acec2444e71
michaellacy <michael.j.lacy@gmail.com>
2026-03-05 18:52:46 -0600

    Fix comma dangle

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import { provideZoneChangeDetection } from "@angular/core";
 import { AppModule } from "./app/app.module";
 import { platformBrowser } from "@angular/platform-browser";
 
-platformBrowser().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection({ eventCoalescing: true })], })
+platformBrowser().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection({ eventCoalescing: true })] })
   .catch(err => console.error(err));
