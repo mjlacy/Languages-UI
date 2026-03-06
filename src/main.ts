82c94bfe5cada269549153e2c45588243b37f265
michaellacy <michael.j.lacy@gmail.com>
2026-03-05 18:52:19 -0600

    Update to Angular 21.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { AppModule } from "./app/app.module";
 import { platformBrowser } from "@angular/platform-browser";
 
-platformBrowser().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true
-})
+platformBrowser().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection({ eventCoalescing: true })], })
   .catch(err => console.error(err));
